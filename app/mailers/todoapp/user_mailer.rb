6e169c76e91ac7de18587960e475a64ecd94affc
ferbin <ferbin17@gmail.com>
2020-10-26 14:43:14 +0530

    feat: email set up

--- app/mailers/todoapp/user_mailer.rb
+++ app/mailers/todoapp/user_mailer.rb
@@ -1,15 +1,17 @@
 module Todoapp
   class UserMailer < ApplicationMailer
+    default from: 'productsmf@gmail.com'
+    
     def send_otp
       @user = params[:user]
       @otp = params[:otp]
-      mail(to: @user.email_id, subject: 'Get Art Sign up OTP')
+      mail(to: @user.email_id, subject: 'Todoapp Sign up OTP')
     end
 
     def send_password_reset_token
       @user = params[:user]
       @user.update(reset_password_sent_at: Time.now, dont_validate_password: false)
-      mail(to: @user.email_id, subject: 'Get Art Password Reset')
+      mail(to: @user.email_id, subject: 'Todoapp Password Reset')
     end
   end
 end
